c38302df0c9d43d9cee2b54a50e14ef0edf0847a
Monika <58700629+KitsunesWrath@users.noreply.github.com>
2020-10-24 16:12:53 +0200

    tests for insertSort - done

--- src/test/java/aisd/lab2/sortcomparison/InsertSortTest.java
+++ src/test/java/aisd/lab2/sortcomparison/InsertSortTest.java
@@ -1,4 +1,72 @@
 package test.java.aisd.lab2.sortcomparison;
 
+import main.java.aisd.lab2.sortcomparison.InsertSort;
+import main.java.aisd.lab2.sortcomparison.QuickSortIterative;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
 public class InsertSortTest {
+    private InsertSort insertSort;
+
+    @Before
+    public void setUp() {
+        insertSort = new InsertSort();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throwIllegalArgumentException_when_vectorIsNull() {
+        // given
+        double[] unsortedVector = null;
+        // when
+        double[] result = insertSort.sort(unsortedVector);
+        // then
+        assert false;
+    }
+
+    @Test
+    public void shouldReturnSortedVector() {
+        //given
+        double[] givenVector = {7, 9, 1, 3};
+        //when
+        double[] result = insertSort.sort(givenVector);
+        //then
+        double[] expected = {1, 3, 7, 9};
+        assertArrayEquals(expected, result, 0);
+    }
+
+    @Test
+    public void shouldNotChange_passedVector() {
+        //given
+        double[] givenVector = {7, 9, 1, 3};
+        //when
+        double[] result = insertSort.sort(givenVector);
+        //then
+        double[] expected = {7, 9, 1, 3};
+        assertArrayEquals(expected, givenVector, 0);
+    }
+
+    @Test
+    public void should_returnEmptyArray_when_VectorIsEmpty() {
+        // given
+        double[] givenVector = {};
+        // when
+        double[] result = insertSort.sort(givenVector);
+        // then
+        double[] expected = {};
+        assertArrayEquals(expected, result, 0);
+    }
+
+    @Test
+    public void shouldDoNothingWithOneElementVector() {
+        //given
+        double[] givenVector = {11};
+        //when
+        double [] result = insertSort.sort(givenVector);
+        //then
+        double [] expected = {11};
+        assertArrayEquals(expected, result, 0);
+    }
 }
--- src/test/java/aisd/lab2/sortcomparison/QuickSortIterativeTest.java
+++ src/test/java/aisd/lab2/sortcomparison/QuickSortIterativeTest.java
@@ -1,4 +1,25 @@
 package test.java.aisd.lab2.sortcomparison;
 
+import main.java.aisd.lab2.sortcomparison.QuickSortIterative;
+import org.junit.Before;
+import org.junit.Test;
+
 public class QuickSortIterativeTest {
+    private QuickSortIterative quickSortIterative;
+
+    @Before
+    public void setUp() {
+        quickSortIterative = new QuickSortIterative();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throwIllegalArgumentException_when_arrayIsNull() {
+        // given
+        double[] unsortedVector = null;
+        // when
+        double[] result = quickSortIterative.sort(unsortedVector);
+        // then
+        assert false;
+    }
 }
+
