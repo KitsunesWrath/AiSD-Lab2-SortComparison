3f54840f6a35dbecef6c5dbd249b3f4467021abd
Monika <58700629+KitsunesWrath@users.noreply.github.com>
2020-10-24 17:11:48 +0200

    tests for QuickSortIterative - done

--- src/test/java/aisd/lab2/sortcomparison/QuickSortIterativeTest.java
+++ src/test/java/aisd/lab2/sortcomparison/QuickSortIterativeTest.java
@@ -25,13 +25,68 @@ public class QuickSortIterativeTest {
     }
 
     @Test
+    public void should_returnEmptyArray_when_VectorIsEmpty() {
+        // given
+        double[] givenVector = {};
+        // when
+        double[] result = quickSortIterative.sort(givenVector);
+        // then
+        double[] expected = {};
+        assertArrayEquals(expected, result, 0);
+    }
+
+    @Test
+    public void shouldDoNothingWithOneElementVector() {
+        //given
+        double[] givenVector = {-11};
+        //when
+        double[] result = quickSortIterative.sort(givenVector);
+        //then
+        double[] expected = {-11};
+        assertArrayEquals(expected, result, 0);
+    }
+
+    @Test
     public void shouldReturnSortedVector() {
         //given
-        double[] givenVector = {7, 9, 1, 3};
+        double[] givenVector = {7, 9, -1, 3};
+        //when
+        double[] result = quickSortIterative.sort(givenVector);
+        //then
+        double[] expected = {-1, 3, 7, 9};
+        assertArrayEquals(expected, result, 0);
+    }
+
+    @Test
+    public void shouldSort_inAscendingOrder() {
+        //given
+        double[] givenVector = {9, 7, 3, 1};
         //when
         double[] result = quickSortIterative.sort(givenVector);
         //then
         double[] expected = {1, 3, 7, 9};
+        assertArrayEquals(expected, result, 0);
+    }
+
+    @Test
+    public void shouldReturnUnchangedVector_ifItIsAlreadySorted() {
+        //given
+        double[] givenVector = {1, 3, 7, 9};
+        //when
+        double[] result = quickSortIterative.sort(givenVector);
+        //then
+        double[] expected = {1, 3, 7, 9};
+        assertArrayEquals(expected, result, 0);
+    }
+
+    @Test
+    public void shouldSortVectorFilledWithIdenticalValues() {
+        //given
+        double[] givenVector = {6, 6, 6};
+        //when
+        double[] result = quickSortIterative.sort(givenVector);
+        //then
+        double[] expected = {6, 6, 6};
         assertArrayEquals(expected, result, 0);
     }
 
