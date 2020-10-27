5de29256bef3f35101de56276dd5b8ba60d614cc
Monika <58700629+KitsunesWrath@users.noreply.github.com>
2020-10-27 18:37:46 +0100

    all done

--- src/main/java/aisd/lab2/sortcomparison/GenerateFile.java
+++ src/main/java/aisd/lab2/sortcomparison/GenerateFile.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 public class GenerateFile {
     GenerateData generateData = new GenerateData();
+
     public void generateFileForInsertSort(int vectorLength) {
         try {
             BufferedWriter dataFile = new BufferedWriter(new FileWriter("src/outputData/InsertSortOutput", true));
@@ -25,6 +26,29 @@ public class GenerateFile {
             double averageAverage = averageCaseTime / 10;
             double worstAverage = worstCaseTime / 10;
             dataFile.write(vectorLength + "  " + bestAverage + "  " + averageAverage + "  " + worstAverage + "\n");
+            dataFile.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void generateFileForQuickSort(int vectorLength) {
+        try {
+            BufferedWriter dataFile = new BufferedWriter(new FileWriter("src/outputData/QuickSortOutput", true));
+            QuickSortIterative quickSortIterative = new QuickSortIterative();
+            double bestCaseTime = 0, worstCaseTime = 0, averageCaseTime = 0;
+            for (int i = 0; i < 10; i++) {
+                double[] bestCase = generateData.generateVectorWithAlmostRandomValues(vectorLength);
+                double[] averageCase = generateData.generateRandomVector(vectorLength);
+                double[] worstCase = generateData.generateVectorWithIdenticalValues(vectorLength);
+                bestCaseTime += generateData.measureExecutionTime(bestCase, quickSortIterative);
+                averageCaseTime += generateData.measureExecutionTime(averageCase, quickSortIterative);
+                worstCaseTime += generateData.measureExecutionTime(worstCase, quickSortIterative);
+            }
+            double bestAverage = bestCaseTime / 10;
+            double averageAverage = averageCaseTime / 10;
+            double worstAverage = worstCaseTime / 10;
+            dataFile.write(vectorLength + " " + bestAverage + "  " + averageAverage + "  " + worstAverage + "\n");
             dataFile.close();
         } catch (IOException ex) {
             ex.printStackTrace();
--- src/main/java/aisd/lab2/sortcomparison/Main.java
+++ src/main/java/aisd/lab2/sortcomparison/Main.java
@@ -1,10 +1,13 @@
 package main.java.aisd.lab2.sortcomparison;
 
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         GenerateFile file = new GenerateFile();
         for (int i = 100; i <= 10000; i += 100) {
+            file.generateFileForQuickSort(i);
             file.generateFileForInsertSort(i);
         }
     }
